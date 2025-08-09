a1103e5a2c8b305683af4370ad854e4afed82ac1
David <fnndolo16@gmail.com>
2025-08-09 16:26:55 -0500

    fix: Update CORS to use environment variable

--- searcher-back/src/main.ts
+++ searcher-back/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['https://tudominio.vercel.app', 'http://localhost:4200'],
+    origin: [process.env.FRONTEND_ORIGIN || 'http://localhost:4200', 'http://localhost:4200'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   });
   const port = process.env.PORT || 3000;
