9af8619e03bcc72aad375f74be9c9e5a176f9354
maskupo.tech <maskupo.tech@gmail.com>
2026-02-10 17:50:14 -0500

    Fix: Importar SharedModule en WebsocketsModule para resolver dependencia de StoreCredentialsService

--- searcher-back/src/websockets/websockets.module.ts
+++ searcher-back/src/websockets/websockets.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { WebsocketsGateway } from './websockets.gateway';
+import { SharedModule } from '../shared/shared.module';
 
 @Module({
+  imports: [SharedModule],
   providers: [WebsocketsGateway],
   exports: [WebsocketsGateway],
 })
